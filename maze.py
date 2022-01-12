8dc58be73784ca6bf3e743568aad5a82c649f46b
Joseph Baker <jbaker@student.neumont.edu>
2022-01-11 18:38:04 -0700

    Maze Piece Guide

---
+++ maze.py
@@ -0,0 +1,13 @@
+#  Maze Piece Guide
+#
+#  Heading
+#    N
+#  W   E
+#    S
+#
+#  0 [filled] - 0 filled tile
+#  1 [straight] - 10 west to east straight path - 11 north to south straight path
+#  2 [special area] - 20 special area opening north - 21 special area opening east - 22 special area opening south - 23 special area opening west
+#  3 [corner] - 30 corner north to east - 31 corner east to south - 32 corner south to west - 33 corner west to north
+#  4 [three way] - 40 three way north to south branch east - 41 three way east to west branch to south - 42 three way south to north branch west - 43 three way west to east branch north
+#  5 [four way] - 50 four way connection
