b6e6c6d0f1bee160c20c324bade50bb7a821ded8
Joseph Baker <jbaker@student.neumont.edu>
2022-01-20 19:55:31 -0700

    Render a Maze
    Adjusted some of the variable names so they better represent what they are connected to.

--- maze.py
+++ maze.py
@@ -14,10 +14,10 @@
 from colorsys import ONE_THIRD, TWO_THIRD
 import random
 from graphics import *
-
 from position import Position
 
 win = GraphWin('Simulaton', 1920/2, 1080/2)  # give title and dimensions
+scaler = 1  # Global scaler
 
 # *Checklist for eventual graphics implementation and collision
 
@@ -48,9 +48,9 @@ class Tile:
         self.sectionPosition.setPosition(Position(sectionX, sectionY))
         self.position.setPosition(Position(tileX, tileY))
 
-    def drawThis(self, scaler):
-        self.drawTile = Rectangle(Point(scaler*(self.position.x), scaler*(self.position.y)), Point(
-            scaler*(self.position.x + (ONE_THIRD * 100)), scaler*(self.position.y + (ONE_THIRD * 100))))
+    def drawThis(self, scale):
+        self.drawTile = Rectangle(Point(scale*(self.position.x), scale*(self.position.y)), Point(
+            scale*(self.position.x + (ONE_THIRD * 100)), scale*(self.position.y + (ONE_THIRD * 100))))
         self.drawTile.setFill('blue')
         self.drawTile.draw(win)
 
@@ -81,79 +81,42 @@ class Section:
         otherTile.walls[Section.pairedWalls[wallPos]] = False
 
     def placeTiles(self):
-        tempID = 00
-        sequence = wallFormations[self.sectionID]
-        # First Row
-        if(sequence.__contains__(1)):
-            T1 = Tile(self.row, self.column,
-                      (ONE_THIRD * 100) + (self.row * 100), (ONE_THIRD * 100) + (self.column * 100))
-            T1.drawThis(1)
-        if(sequence.__contains__(2)):
-            T2 = Tile(self.row, self.column,
-                      (TWO_THIRD * 100) + (self.row * 100), (ONE_THIRD * 100) + (self.column * 100))
-            T2.drawThis(1)
-        if(sequence.__contains__(3)):
-            T3 = Tile(self.row, self.column,
-                      100 + (self.row * 100), (ONE_THIRD * 100) + (self.column * 100))
-            T3.drawThis(1)
-        # Second Row
-        if(sequence.__contains__(4)):
-            T4 = Tile(self.row, self.column, (ONE_THIRD * 100) +
-                      (self.row * 100), (TWO_THIRD * 100) + (self.column * 100))
-            T4.drawThis(1)
-        if(sequence.__contains__(5)):
-            T5 = Tile(self.row, self.column, (TWO_THIRD * 100) +
-                      (self.row * 100), (TWO_THIRD * 100) + (self.column * 100))
-            T5.drawThis(1)
-        if(sequence.__contains__(6)):
-            T6 = Tile(self.row, self.column, 100 + (self.row * 100),
-                      (TWO_THIRD * 100) + (self.column * 100))
-            T6.drawThis(1)
-        # Thrid Row
-        if(sequence.__contains__(7)):
-            T7 = Tile(self.row, self.column, (ONE_THIRD * 100) +
-                      (self.row * 100), 100 + (self.column * 100))
-            T7.drawThis(1)
-        if(sequence.__contains__(8)):
-            T8 = Tile(self.row, self.column, (TWO_THIRD * 100) +
-                      (self.row * 100), 100 + (self.column * 100))
-            T8.drawThis(1)
-        if(sequence.__contains__(9)):
-            T9 = Tile(self.row, self.column, 100 +
-                      (self.row * 100), 100 + (self.column * 100))
-            T9.drawThis(1)
-
-
-testing = Section(0, 0)
-testing.placeTiles()
-testing2 = Section(0, 1)
-testing2.placeTiles()
-testing3 = Section(1, 0)
-testing3.placeTiles()
+        sequence = wallFormations[int(self.sectionID)]
+        operations = [(100/3), (200/3), (100)]
+        count = 1
+        # Go through each row, column by column to render the tile
+        # If the sequence contains the count
+        for col in range(operations.__len__()):
+            for row in range(operations.__len__()):
+                if(sequence.__contains__(count)):
+                    T = Tile(self.row, self.column, (operations[col] + (
+                        self.row * 100)), (operations[row] + (self.column * 100)))
+                    T.drawThis(scaler)
+                count = count + 1
 
 
 class Maze:
 
-    def __init__(self, yHeight, xLength):
+    def __init__(self, yLength, xHeight):
         # Size of defined maze height (y) by length (x)
-        self.lengthX = xLength
-        self.heightY = yHeight
+        self.heightX = xHeight
+        self.lengthY = yLength
 
         # Defined start position at (0, 0)
         self.currentX = 0
         self.currentY = 0
 
         # Create 2D array to represent a map of the maze (contains sections)
-        self.mazeMap = [[Section(x, y) for y in range(yHeight)]
-                        for x in range(xLength)]
+        self.mazeMap = [[Section(x, y) for y in range(yLength)]
+                        for x in range(xHeight)]
 
     def __str__(self):
         mazeRows = ['']
         foundStart = 'Not Found'
         foundEnd = 'Not Found'
-        for y in range(self.heightY):
+        for x in range(self.heightX):
             mazeRow = ['']
-            for x in range(self.lengthX):
+            for y in range(self.lengthY):
                 wallSections = [""]
                 currentSection = self.mazeMap[x][y]
                 if(currentSection.walls['N']):
@@ -168,10 +131,10 @@ class Maze:
                     ''.join(wallSections)))
                 if(currentSection.goal.__contains__("Start")):
                     foundStart = currentSection.goal + \
-                        " (" + str(y) + ", " + str(x) + ")"
+                        " (" + str(x) + ", " + str(y) + ")"
                 if(currentSection.goal.__contains__("End")):
                     foundEnd = currentSection.goal + \
-                        " (" + str(y) + ", " + str(x) + ")"
+                        " (" + str(x) + ", " + str(y) + ")"
 
             mazeRows.append(''.join(mazeRow))
         mazeRows.append(foundStart)
@@ -179,8 +142,14 @@ class Maze:
 
         return '\n'.join(mazeRows)
 
+    def renderMaze(self):
+        for x in range(self.heightX):
+            for y in range(self.lengthY):
+                currentSection = self.mazeMap[x][y]
+                currentSection.placeTiles()
+
     def generateMaze(self):
-        totalSections = self.lengthX * self.heightY
+        totalSections = self.heightX * self.lengthY
         sectionStack = []
         currentSection = self.sectionAt(self.currentX, self.currentY)
         vistedSections = 1
@@ -203,9 +172,27 @@ class Maze:
             sectionStack.append(currentSection)
             currentSection = nextSection
             vistedSections += 1
+        # Assign section IDs
+        self.assignSectionID()
 
     def sectionAt(self, x, y):
         return self.mazeMap[x][y]
+
+    def assignSectionID(self):
+        for x in range(self.heightX):
+            for y in range(self.lengthY):
+                wallSections = [""]
+                currentSection = self.mazeMap[x][y]
+                if(currentSection.walls['N']):
+                    wallSections.append("N")
+                if(currentSection.walls['E']):
+                    wallSections.append("E")
+                if(currentSection.walls['S']):
+                    wallSections.append("S")
+                if(currentSection.walls['W']):
+                    wallSections.append("W")
+                currentSection.sectionID = self.sectionWallsToPieceID(
+                    ''.join(wallSections))
 
     def sectionWallsToPieceID(self, walls):
         # Given a set of walls, return what piece that would represent
@@ -263,7 +250,7 @@ class Maze:
             # A valid neighbour is identified if the final displacement is
             # Greater than 0
             # Less than max length/height
-            if (0 <= xFinal < self.lengthX) and (0 <= yFinal < self.heightY):
+            if (0 <= xFinal < self.heightX) and (0 <= yFinal < self.lengthY):
                 neighbour = self.sectionAt(xFinal, yFinal)
                 # Check if we have been there before
                 # If not add it to the list
@@ -275,13 +262,14 @@ class Maze:
 
 # Size Definition of the maze
 height = 5
-length = 6
+length = 9
 
 # Maze Generation and display
-generatedMaze = Maze(height, length)
+generatedMaze = Maze(length, height)
 generatedMaze.generateMaze()
-# print(generatedMaze)
+print(generatedMaze)
+generatedMaze.renderMaze()
 
-window = True
-while(window):
-    if(keyboard)
+inc = 0
+while(True):
+    inc = inc + 1
