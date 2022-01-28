78a243e57dd5e414419d7e4f79525ca3a1567fda
Joseph Baker <jbaker@student.neumont.edu>
2022-01-27 18:41:40 -0700

    To Be Tested

--- ant.py
+++ ant.py
@@ -4,6 +4,7 @@ import math as m
 # import random
 from maze import *
 
+
 class Path:
     posList = []
     time = 0.0
@@ -20,6 +21,7 @@ class Path:
 
 allAnts = []
 
+
 class AntModel:
 
     path = []
@@ -30,10 +32,10 @@ class AntModel:
     antDraw = None
     win = None
 
-    def __init__(self, position, window, allTiles, sumOfSections):
+    def __init__(self, position, endPosition, window, allTiles, sumOfSections):
 
         self.colliding = True
-        self.currentPos = (Position(0,0))
+        self.currentPos = (Position(0, 0))
 
         self.sumOfSections = sumOfSections
 
@@ -42,6 +44,7 @@ class AntModel:
 
         #print("I am an ant")
         self.currentPos.setPosition(position)
+        self.endPosition = endPosition
         self.path = Path(self.currentPos)
 
         self.antDraw = Circle(Point(self.currentPos.x, self.currentPos.y), 5)
@@ -77,7 +80,8 @@ class AntModel:
 
         # print(self.currentPos.y+aside)
 
-        self.collisionNode = Position(self.currentPos.x+bside, self.currentPos.y+aside)
+        self.collisionNode = Position(
+            self.currentPos.x+bside, self.currentPos.y+aside)
         shouldMove = True
         # collisionDraw = None
 
@@ -87,11 +91,12 @@ class AntModel:
 
         # print(len(allTiles))
 
-        self.currentSegment = (m.floor(scale*((self.currentPos.x-100/3)/100)) + (m.floor(scale*((self.currentPos.y-100/3)/100))*length))
+        self.currentSegment = (m.floor(scale*((self.currentPos.x-100/3)/100)) +
+                               (m.floor(scale*((self.currentPos.y-100/3)/100))*length))
 
         # print(currentSegment)
         # time.sleep(0.05)
-        
+
         # print(str(self.currentSegment) + " x: " + str(m.floor(scale*((self.currentPos.x-100/3)/100))) + " y: " + str(m.floor(scale*((self.currentPos.y)/100))*length))
         i = 0
         while i < len(allSectionsTiles[self.currentSegment]):
@@ -101,12 +106,60 @@ class AntModel:
                 self.rotation = 360*random.random()
                 self.setRotation()
 
-                i=9999
-            i+=1
-            
+                i = 9999
+            i += 1
+
         if self.colliding == False:
             self.antDraw.move(bside, aside)
             self.currentPos.movePosition(bside, aside)
-            
+            # Checks if the ant has reached the end special area
+            self.checkIfEnd(self.endPosition)
+            if(self.objective == True):
+                self.getNextNode()
+                # Needs to return something
+                self.calcAntBias()
             # print(allTiles[i].position.x)
 
+    def writeToPath(self):
+        self.path.add_node(self.currentPos)
+
+    def checkBestPath(self):
+        bestPath = getBestPath()
+        if(bestPath != 0 and bestPath.getTime() > self.path.getTime()):
+            bestPath = self.path
+            objectiveFound = True
+
+    def getNextNode(self):
+        closestNodesList = []
+        for i in range(bestPath.size()):
+            xdis = self.currentPos.x - bestPath[i].x
+            ydis = self.currentPos.y - bestPath[i].y
+            totaldisSqr = (xdis * xdis) + (ydis * ydis)
+            if(m.sqrt(totaldisSqr) < 5 and self.nextNode <= i):
+                closestNodesList.append(bestPath[i])
+                self.nextNode = closestNodesList[closestNodesList.size(
+                ) - 1][1] + 1
+
+    def calcAntBias(self):
+        self.nextNode
+        xdis = self.currentPos.x - self.nextNode[0].x
+        ydis = self.currentPos.y - self.nextNode[0].y
+        result = m.atan2(ydis, xdis) * 180/m.pi
+        # totaldisSqr = (xdis * xdis) + (ydis * ydis)
+        # totaldis = m.sqrt(totaldisSqr)
+
+    def checkIfEnd(self, endPos):
+        self.currentSegment = (m.floor(scale*((self.currentPos.x-100/3)/100)),
+                               (m.floor(scale*((self.currentPos.y-100/3)/100))))
+        # must compare center tile
+        print(str(endPos.x) + " " + str(endPos.y))
+
+        if ((endPos.x - 50/3) < self.currentPos.x and (endPos.x + 50/3) > self.currentPos.x and (endPos.y - 50/3) < self.currentPos.y and (endPos.y + 50/3) > self.currentPos.y):
+            print("An ant has reached the end")
+            self.checkBestPath()
+            return True
+        else:
+            return False
+
+    def antSpawn(self, startPos):
+        random.randint(0, startPos.x)
